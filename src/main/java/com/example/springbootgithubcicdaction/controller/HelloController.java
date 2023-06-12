9fa8ff49f4323414409739492e2502d0b03024ff
Kumar Vijay <vijay81238@gmail.com>
2023-06-12 15:43:17 +0530

    Triggering githib action

--- src/main/java/com/example/springbootgithubcicdaction/controller/HelloController.java
+++ src/main/java/com/example/springbootgithubcicdaction/controller/HelloController.java
@@ -13,6 +13,7 @@ public class HelloController {
     @GetMapping("/test")
     public String test(){
         System.out.println("Test SOP");
+        System.out.println("Test1 SOP");
         return "This is spring boot github action project!";
     }
 }
